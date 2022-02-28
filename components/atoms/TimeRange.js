968c3e654f2442e38635276b28176ae9fb668f89
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2022-02-28 11:11:16 +0100

    push fix

---
+++ components/atoms/TimeRange.js
@@ -0,0 +1,27 @@
+import { Select } from "antd";
+
+ const { Option } = Select;
+
+const TimeRange = ({ setTime, time}) => {
+  return (
+    <div className="flex justify-between items-center w-56 mt-2">
+      <label htmlFor="timeperiod" className="text-xl">
+        {" "}
+        Set time range:
+      </label>
+      <Select
+	  id="timeperiod"
+        defaultValue="7d"
+        className="select-timeperiod"
+        placeholder="Select Timeperiod"
+        onChange={(value) => setTime(value)}
+      >
+        {time.map((date) => (
+          <Option key={date}>{date}</Option>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+export default TimeRange;
