072a2f72c85388c298ea3ff382a94a0d47851de1
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2022-03-03 13:44:13 +0100

    implement state managament to control sidebar

--- components/atoms/Navbar-Item/NavbarItem.js
+++ components/atoms/Navbar-Item/NavbarItem.js
@@ -1,13 +1,25 @@
 import React from "react";
 
+//redux state management
+import { toggleNavBar } from "../../../redux/navBarState";
+import { useDispatch } from "react-redux";
+
 const NavbarItem = ({ children, icon }) => {
+  const dispatch = useDispatch();
+
   return (
     <li>
-      <span className="relative my-1 flex flex-row items-center h-11 focus:outline-none hover:bg-[#111827] rounded-md text-white-600 hover:!text-white-800 border-l-4 border-transparent cursor-pointer pr-6">
-        <span className="inline-flex justify-center items-center ml-2">
+      <span className="relative my-1 flex flex-row items-center h-11 focus:outline-none hover:bg-[#111827] rounded-md text-white-600 hover:!text-white-800 border-l-4 border-transparent cursor-pointer pr-6 pl-1">
+        <span className="inline-flex justify-center items-center ml-1">
           {icon}
         </span>
-        <span className="ml-3 text-sm font-semibold capitalize tracking-wide truncate">
+        <span
+          className="ml-2 text-sm font-semibold capitalize tracking-wide truncate"
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(toggleNavBar());
+          }}
+        >
           {children}
         </span>
         {/* <span className="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">
--- components/molecules/CryptoStats.js
+++ components/molecules/CryptoStats.js
@@ -3,7 +3,7 @@ import { Col, Statistic, Card } from "antd";
 
 const CryptoStats = ({title, value}) => {
 	return (
-    <Col lg={8} sm={24} className="mb-1 lg:mb-0">
+    <Col lg={8} sm={24} xs={24} className="mb-1 lg:mb-0">
       <Card
         hoverable
         className="break-words bg-white dark:bg-gray-800 shadow-md rounded transform duration-300 ease-linear delay-150"
